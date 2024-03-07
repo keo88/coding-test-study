e89fba8e0bb69d3e1780e5db5ec7c08191742831
keo88 <keo.kim0808@gmail.com>
2024-03-07 15:29:11 -0600

    solve: LC-Binary Tree Right Side View.py

---
+++ 김기오/LC-Binary Tree Right Side View.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        q = deque([(root, 0)])
+        arr = []
+
+        while q:
+            node, depth = q.popleft()
+            if depth == len(arr):
+                arr.append(node.val)
+            
+            if node.right:
+                q.append((node.right, depth + 1))
+
+            if node.left:
+                q.append((node.left, depth + 1))
+        return arr
