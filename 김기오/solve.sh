bf5ab256d9aa1ab55da90607a61624302457fac0
keo88 <keo.kim0808@gmail.com>
2024-03-06 21:08:53 -0600

    chore: edit solve.sh so that it takes title of the problems from start.

--- 김기오/solve.sh
+++ 김기오/solve.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Step 1: Take leetcode problem page url as input and extract the title
-read -p "Enter the LeetCode problem page URL: " leetcode_url
+# read -p "Enter the LeetCode problem page URL: " leetcode_url
+read -p "Enter the LeetCode problem name: " title
 
 # Extracting the title from the URL
-title=$(curl -s "$leetcode_url" | grep -o '<title>[^<]*' | sed -e 's/<title>//' -e 's/ - LeetCode$//')
+# title=$(curl -s "$leetcode_url" | grep -o '<title>[^<]*' | sed -e 's/<title>//' -e 's/ - LeetCode$//')
 
 # Step 2: Create a Python file with the extracted title
 file_name="LC-${title}.py"
