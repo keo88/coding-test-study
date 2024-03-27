3e4b747d11460aebdf800cd329143cc9fc5dd4ed
keo88 <keo.kim0808@gmail.com>
2024-03-27 15:41:26 +0900

    solve: lv3 연속 펄스 부분 수열의 합.py
    review: Kadane's algorithm

--- keo88/lv3 연속 펄스 부분 수열의 합.py
+++ keo88/lv3 연속 펄스 부분 수열의 합.py
@@ -1,8 +1,20 @@
 def solution(sequence):
-    answer = 0
+    
+    # Sol 1. 누적합
     acc = [0]
     mult = 1
     for i in range(len(sequence)):
         acc.append(acc[i] + sequence[i] * mult)
         mult *= -1
     return max(acc) - min(acc)
+    
+    # Sol 2. 카데인
+    dp = [[sequence[0], -sequence[0]]]
+
+    for i in range(1, len(sequence)):
+        tup = []
+        for j in range(2):
+            mult = -1 if (i + j) % 2 else 1
+            tup.append(max(dp[i - 1][j] + sequence[i] * mult, sequence[i] * mult))
+        dp.append(tup)
+    return max(item for t in dp for item in t)
